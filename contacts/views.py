c813819959aab67542b487ce1fab5711866535f7
deepssathyan <deepssathyan0613@gmail.com>
2023-12-13 22:47:16 -0800

    List feature added

--- contacts/views.py
+++ contacts/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
 from .forms import ContactForm
 
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
