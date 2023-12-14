7f04f7b22928acf548751ec4b201ee83550ae7c8
deepssathyan <deepssathyan0613@gmail.com>
2023-12-13 22:18:20 -0800

    added forms.py

---
+++ contacts/forms.py
@@ -0,0 +1,9 @@
+# contacts/forms.py
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'notes')
