281ab208bb22d7403eff44281a6162de1c2b16ab
deepssathyan <deepssathyan0613@gmail.com>
2023-12-13 22:48:14 -0800

    Details feature added

--- contacts/views.py
+++ contacts/views.py
@@ -6,3 +6,7 @@ def contact_list(request):
     contacts = Contact.objects.all()
     return render(request, 'contacts/contact_list.html', {'contacts': contacts})
 
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contacts/contact_detail.html', {'contact': contact})
+
