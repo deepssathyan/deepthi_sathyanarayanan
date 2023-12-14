ab8e69fa44d808a623d2b35b7b0fa9a7f27ba5f9
deepssathyan <deepssathyan0613@gmail.com>
2023-12-13 22:48:57 -0800

    New contact feature added

--- contacts/views.py
+++ contacts/views.py
@@ -10,3 +10,14 @@ def contact_detail(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     return render(request, 'contacts/contact_detail.html', {'contact': contact})
 
+def contact_new(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/contact_edit.html', {'form': form})
+
